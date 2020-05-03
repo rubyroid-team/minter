b36d880f55db881d2cc15a7b2dbd9702f5583e11
Alexander Nikolaev <ni-ko-la@mail.ru>
2020-05-03 06:10:11 +0300

    add comment to transactionFfi with usage

--- lib/ffi/transaction_ffi.rb
+++ lib/ffi/transaction_ffi.rb
@@ -11,3 +11,40 @@ module Minter
     attach_function :SignCreateCoinTransaction, [:string], :string
   end
 end
+
+
+# ===SEND MNT===
+# tx_params = {
+#     Nonce: 27,
+#     ChainId: 2,
+#     Type: 1,
+#     AddressTo: "Mx7633980c000139dd3bd24a3f54e06474fa941e16",
+#     Value: 24_000_000_000_000_000_000,
+#     Coin: "MNT",
+#     GasCoin: "MNT",
+#     GasPrice: 10,
+#     PrivateKey: "290cd647206bea71ec06d8dfacce30c872aea3fb1ea333b7f475b70467250ca0"
+# }
+# tx = Minter::TransactionFfi.SignTransaction(tx_params.to_json)
+# Minter::Api::Client.new.send_transaction(transaction: tx)
+
+
+
+# ==CREATE COIN==
+# tx_params = {
+#     Name: "DEVTWO",
+#     Symbol: "DEVTWO",
+#     InitialAmount: 1000000000000000000,
+#     MaxSupply: 10000000000000000000,
+#     InitialReserve: 10000,
+#     ReserveRation: 10,
+#
+#     Nonce: 29,
+#     ChainId: 2,
+#     GasCoin: "MNT",
+#     GasPrice: 1,
+#     PrivateKey: "290cd647206bea71ec06d8dfacce30c872aea3fb1ea333b7f475b70467250ca0"
+# }
+#
+# tx = Minter::TransactionFfi.SignCreateCoinTransaction(tx_params.to_json)
+# Minter::Api::Client.new.send_transaction(transaction: tx)
