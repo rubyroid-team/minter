ae2f4f5fc7e397af4fd10ede4eef120a9ec3d247
Alexander Nikolaev <ni-ko-la@mail.ru>
2020-05-25 16:06:10 +0300

    clear ffi/sign build

--- lib/ffi/sign/sign-darwin-10.6-amd64.h
+++ lib/ffi/sign/sign-darwin-10.6-amd64.h
@@ -71,34 +71,6 @@ extern "C" {
 
 extern char* Sign(char* p0);
 
-extern char* SignCreateCoinTransaction(char* p0);
-
-extern char* SignSellCoinTransaction(char* p0);
-
-extern char* SignBuyCoinTransaction(char* p0);
-
-extern char* SignSellAllCoinTransaction(char* p0);
-
-extern char* SignDeclareCandidacyTransaction(char* p0);
-
-extern char* SignDelegateTransaction(char* p0);
-
-extern char* SignUnbondTransaction(char* p0);
-
-extern char* SignSetCandidateOffTransaction(char* p0);
-
-extern char* SignSetCandidateOnTransaction(char* p0);
-
-extern char* SignRedeemCheckTransaction(char* p0);
-
-extern char* SignEditCandidateTransaction(char* p0);
-
-extern char* SignMultiSendTransaction(char* p0);
-
-extern char* SignCreateMultisigAddressTransaction(char* p0);
-
-extern char* DecodeTransaction(char* p0);
-
 #ifdef __cplusplus
 }
 #endif
